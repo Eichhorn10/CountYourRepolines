428a5559bbab001d6957c22a1e01ae5d44c73eaa
Eichhorn10 <danielmass03@gmail.com>
2023-02-25 00:04:31 +0100

    Final Commit

--- CountLines.py
+++ CountLines.py
@@ -1,64 +1,8 @@
 import requests
 
-
-# count = 0
-# with open('Desktop/git.txt') as file:
-#     for line in file:
-#         if line.strip():
-#             count += 1
-
-# print('Number of Lines', count)
-
-# Holen Sie sich eine Liste von Dateien im Repository
-# response = requests.get(url, params={'ref': branch})
-# data = response.json()
-
-# # Initialisieren Sie den Zeilenzähler
-
-# # Durchlaufen Sie jede Datei und jeden Ordner im src-Verzeichnis
-# for file_or_folder in data:
-#     # Ignoriere Dateien, die keine Code-Dateien sind
-#     if file_or_folder['type'] == 'file' and not file_or_folder['name'].endswith('.ts'):
-#         continue
-#     # Wenn es sich um einen Ordner handelt, rufen Sie die API auf, um die Dateien im Ordner abzurufen
-#     if file_or_folder['type'] == 'dir':
-#         subdir_url = file_or_folder['url']
-#         subdir_response = requests.get(subdir_url, params={'ref': branch})
-#         subdir_data = subdir_response.json()
-#         # Durchlaufen Sie jede Datei im Unterordner und zählen Sie die Zeilen
-#         for file in subdir_data:
-#             # Ignoriere Dateien, die keine Code-Dateien sind
-#             if file['type'] == 'file' and not file['name'].endswith(('.ts', '.py', '.java', '.c', '.cpp', '.h')):
-#                 continue
-#             if file['type'] == 'dir':
-#                 if file_or_folder['type'] == 'dir':
-#                     subdir_url = file_or_folder['url']
-#                     subdir_response = requests.get(subdir_url, params={'ref': branch})
-#                     subdir_data = subdir_response.json()
-#                     # Durchlaufen Sie jede Datei im Unterordner und zählen Sie die Zeilen
-#                     for file in subdir_data:
-#                         # Ignoriere Dateien, die keine Code-Dateien sind
-#                         if file['type'] == 'file' and not file['name'].endswith(('.ts', '.py', '.java', '.c', '.cpp', '.h')):
-#                             continue
-#                         # Zählen Sie die Zeilen in der Datei
-#                         file_url = file['download_url']
-#                         line_count += count_lines(file_url)
-#             # Zählen Sie die Zeilen in der Datei
-#             file_url = file['download_url']
-#             line_count += count_lines(file_url)
-#     else:
-#         # Wenn es sich um eine Datei handelt, zählen Sie die Zeilen in der Datei
-#         file_url = file_or_folder['download_url']
-#         line_count += count_lines(file_url)
-# # Gib die Gesamtzahl der Zeilen aus
-# print(f"Das Repository hat insgesamt {line_count} Zeilen.")
-
 line_count = 0
 
-url = 'https://api.github.com/repos/TalentLayer/talentlayer-id-subgraph/contents'
-branch = 'main'
-
-# Definieren Sie eine Funktion, um die Anzahl der Zeilen in einer Datei zu zählen
+# Counts all written lines
 def count_lines(file_url):
     count = 0
     file_response = requests.get(file_url)
@@ -68,23 +12,58 @@ def count_lines(file_url):
             count += 1
     return count
 
-def searchFolder(_url, _branch):
+def searchFileOrFolder(_url, _branch):
     global line_count
+    global ignore_files
+    global ignore_filetypes
 
     response = requests.get(_url, params={'ref': _branch})
     data = response.json()
-    if (response.status_code == 403):
-        print("Wir sind am rate limit.")
-        exit()
+    match response.status_code:
+        case 403:
+            print("Wir sind am rate limit.")
+            exit()
+        case 404:
+            print("The inserted URL does not exist. Try another URL.")
+            return
+        case _: pass
 
     for file_or_folder in data:
-        if file_or_folder['type'] == 'file' and not file_or_folder['name'].endswith('.ts'):
+        if file_or_folder['type'] == 'file' and (((ignore_files != ('',)) and (file_or_folder['name'] in ignore_files)) or ((ignore_filetypes != ('',)) and (file_or_folder['name'].endswith(ignore_filetypes)))):
             continue
         if file_or_folder['type'] == 'dir':
-            searchFolder(file_or_folder['url'], _branch)
+            searchFileOrFolder(file_or_folder['url'], _branch)
             continue
         file_url = file_or_folder['download_url']
         line_count += count_lines(file_url)
 
-searchFolder(url, branch)
-print(f"Das Repository hat insgesamt {line_count} Zeilen.")
+while True:
+    print("Please read the README.md file before using!!\n")
+
+    print("Which repository should I count?")
+    url = input("URL: ").replace("github.com", "api.github.com/repos") + "/contents"
+    branch = 'main'
+    with open("..\CountYourRepolines\ignoreFiles.txt", "r") as file:
+        ignore_files = tuple(file.read().split(','))
+    file.close()
+    with open("..\CountYourRepolines\ignoreFiletypes.txt", "r") as file:
+        ignore_filetypes = tuple(file.read().split(','))
+    file.close()
+
+    searchFileOrFolder(url, branch)
+    print(f"The repository uses {line_count} lines of code.")
+    if ignore_files != ('',):
+        print("Ignored files: " + str(ignore_files).replace(',', ''))
+    if ignore_filetypes != ('',):
+        print("Ignored filetypes: " + str(ignore_filetypes).replace(',', ''))
+    print("")
+
+    while True:
+        match input("Try another repository? (y/n)\n"):
+            case "y":
+                line_count = 0
+                break
+            case "n":
+                exit()
+            case _:
+                print("Wrong input, try again.")
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # CountYourRepolines
- When started, it counts all your written codelines.
+## Description
+It's a simple Python-Script that counts all written codelines of your repository.
+
+## Steps
+1. **[Optionaly]** Note all files you don't want to be counted in the *'IgnoreFiles.txt'*-file.
+2. **[Optionaly]** Note all filetypes you don't want to be counted into the *'IgnoreFiletypes.txt'*-file.
+3. Insert your repository-URL that you want to be counted.
+4. Let the magic begin :sparkles:
+
+## Syntax
+All ignored files and filetypes have to be seperated with a comma.
+
+### ***Example:***
+*IgnoreFile.txt:* 'README.md,IgnoreFiletypes.txt'
+
+*IgnoreFiletypes.txt:* '.md,.txt'
